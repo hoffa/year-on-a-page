8bc1b9acc78d3b1567d9b0de548b43642fc6c2b7
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-06-10 10:25:44 +0900

    Update render

--- render.sh
+++ render.sh
@@ -5,3 +5,4 @@ python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt
 .venv/bin/python main.py --year 2024 > 2024.svg
 .venv/bin/python main.py --year 2025 > 2025.svg
+.venv/bin/python main.py --year 2026 > 2026.svg
