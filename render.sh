d19fceda9f94c8f17846edf4de41c2e1033c113c
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-12-29 21:47:16 +0900

    Add PDF

--- render.sh
+++ render.sh
@@ -6,5 +6,7 @@ python3 -m venv .venv
 for year in 2024 2025 2026; do
     for variant in default month monthkorean emoji; do
         .venv/bin/python main.py --year "$year" --variant "$variant" > "renders/$year-$variant.svg"
+        # A4 PDF
+        .venv/bin/python -c "import cairosvg; cairosvg.svg2pdf(url='renders/$year-$variant.svg', write_to='renders/pdf/$year-$variant.pdf', output_width=595, output_height=842)"
     done
 done
