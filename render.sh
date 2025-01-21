a0d453ba0539a67aba5cca84a7db6685b9983be5
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2025-01-21 21:43:11 +0900

    Add latest

--- render.sh
+++ render.sh
@@ -8,3 +8,5 @@ for year in 2024 2025 2026; do
 	# A4 PDF
 	.venv/bin/python -c "import cairosvg; cairosvg.svg2pdf(url='renders/svg/$year-default.svg', write_to='renders/pdf/$year-default.pdf', output_width=595, output_height=842)"
 done
+
+.venv/bin/python main.py --year 2025 --highlight-past >"renders/svg/latest.svg"
